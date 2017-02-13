fbacb049ebaecdc63eb2db37b8b604fc01fa17fe
Jeanette Phung <j5phung@ucsd.edu>
2017-02-13 13:49:32 -0800

    updated json template variables

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,13 @@
 var data = require('../data.json');
 
-exports.view = function(req, res){
-  res.render('profile', data);
+//exports.view = function(req, res){
+//  res.render('profile', data);
+//};
+
+exports.viewProject = function(req, res) {
+	var name = req.params.name;
+	console.log("The project name is: " + name);
+	res.render('project', {
+			'projectName' : name
+	});
 };
