16bfda1c3b75305211c3f131602b6961816da5b0
Jeanette Phung <j5phung@ucsd.edu>
2017-02-13 13:55:34 -0800

    fixed broken code

--- routes/profile.js
+++ routes/profile.js
@@ -1,13 +1,13 @@
 var data = require('../data.json');
 
-//exports.view = function(req, res){
-//  res.render('profile', data);
-//};
-
-exports.viewProject = function(req, res) {
-	var name = req.params.name;
-	console.log("The project name is: " + name);
-	res.render('project', {
-			'projectName' : name
-	});
+exports.view = function(req, res){
+  res.render('profile', data);
 };
+
+//exports.viewProject = function(req, res) {
+//	var name = req.params.name;
+//	console.log("The project name is: " + name);
+//	res.render('project', {
+//			'projectName' : name
+//	});
+//};
