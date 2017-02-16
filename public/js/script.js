75332af3c963b92a8d03bf0ec7f1e1e263116c1d
Rui Jiang <juneruijiang@gmail.com>
2017-02-16 02:18:30 -0800

    add logout and fix validation

--- public/js/script.js
+++ public/js/script.js
@@ -13,37 +13,20 @@ function initializePage() {
 		return this.optional(element) || /^\d{3}-\d{3}-\d{4}$/.test(value);
 	}, "Please enter a valid phone number");
 
-	$("#contact_form").validate({
+	$('#contact_form').validate({
 		rules: {
-			contact_name: "required",
-			contact_phone: {
-				required: true
-			},
 			contact_email: {
-				required: true,
 				email: true
-			},
-			contact_message: {
-				required: true
 			}
 		},
 		messages: {
-			contact_name: "Please enter your name",
-			contact_phone: {
-				required: "Please enter your phone number(XXX-XXX-XXXX)",
-			},
 			contact_email: {
-				required: "Please enter your email address",
 				email: "Please enter a valid email address"
-			},
-			contact_message: {
-				required: "Your message is empty",
 			}
 		}
 	});
 
 	$("#contactUs_submit").click(function () {
-		$("#contact_form").submit();
-		return false;
+		$('#contact_form').submit();
 	})
 }
