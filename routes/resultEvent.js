9e9c4570f6a105ad600caaf2383019e3c915a31a
Jeanette Phung <j5phung@ucsd.edu>
2017-02-20 22:41:13 -0800

    rename events (remove create and join distinctions) and fix bug between unjoin and search

--- routes/resultEvent.js
+++ routes/resultEvent.js
@@ -1,24 +1,32 @@
 var events = require('../events.json');	
-var results = [];
 
-for (var i = 0; i < events.length; i++){
-	if (events[i].created == false && events[i].joined == false){
-		results.push(events[i]);
+function getResult() {
+	var results = [];
+
+	for (var i = 0; i < events.length; i++){
+		if (events[i].created == false && 	events[i].joined == false){
+			results.push(events[i]);
+		}
 	}
+	
+	return results;	
 }
 
 exports.view = function(req, res){
+	var results = getResult();
 	res.render('resultEvent', results);
 };
 
 exports.categoryView = function(req, res){
 	var filter = [];
+	var results = getResult();
 	var category = req.params.category;		 	
-	
+
 	for (var i = 0; i < results.length; i++){
 		if (results[i].category == category){
 			filter.push(results[i]);
 		}
 	}
+
 	res.render('resultEvent', filter);
 };
