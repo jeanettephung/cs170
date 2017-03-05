985afcc8134cf8342a00e5d89fb8509d4cd75165
Jeanette Phung <j5phung@ucsd.edu>
2017-03-05 11:34:30 -0800

    fixed a/b page switch

--- routes/index_A.js
+++ routes/index_A.js
@@ -3,10 +3,10 @@ var data = require('../events.json');
 exports.viewA = function(req, res){
 		data["variant"] = false;
   	res.render('index_A', {event: data, layout: false});
+	console.log(data);
 };
 
 exports.viewB = function(req, res){
-	var data = {events};
 		data["variant"] = true;
-		res.render('index_B', {event: data, layout: false});
+		res.render('index_A', {event: data, layout: false});
 };
